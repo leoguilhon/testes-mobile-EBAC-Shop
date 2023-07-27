df323ca9672ff9f176d983d2c88f84d6a1dfe5b0
Leonardo Guilhon <leoguilhon@gmail.com>
2023-07-26 21:33:22 -0300

    Primeiro Teste Mobile

---
+++ test/specs/login.spec.js
@@ -0,0 +1,7 @@
+describe('Access Admin Panel', () => {
+    it('should login with valid credentials',  async () => {
+        setTimeout(()=>{
+            console.log('abriu')
+        }, 10000)
+    });
+})
